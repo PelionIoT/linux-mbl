torvalds/linux

MLK-13387-2 regulator: consumer: add new event macro
Add the AFT_ENABLE event macros, because that
1.8v of imx7d pcie phy, should be turned on after
the 1p0d(1.0v) of pcie phy is turned on.

Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>
(cherry picked from commit 651954def2e2fd1194f09159477fcee1a6c55a85)

include/linux/regulator/consumer.h
@@ -121,6 +121,7 @@ struct regmap;
 #define REGULATOR_EVENT_ABORT_DISABLE		0x800
 #define REGULATOR_EVENT_PRE_DO_ENABLE		0x1000
 #define REGULATOR_EVENT_PRE_DO_DISABLE		0x2000
+#define REGULATOR_EVENT_AFT_DO_ENABLE		0x4000
 
 /**
  * struct pre_voltage_change_data - Data sent with PRE_VOLTAGE_CHANGE event

PelionIoT/linux-mbl

include/linux/regulator/consumer.h
@@ -123,6 +123,7 @@ struct regmap;
 #define REGULATOR_EVENT_ENABLE			0x1000
 #define REGULATOR_EVENT_PRE_DO_ENABLE		0x2000
 #define REGULATOR_EVENT_PRE_DO_DISABLE		0x4000
+#define REGULATOR_EVENT_AFT_DO_ENABLE		0x8000
 
 /*
  * Regulator errors that can be queried using regulator_get_error_flags